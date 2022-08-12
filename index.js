060d6431c407d44750be824f9299620223d8bfa1
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-12 10:24:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+	let message = [];
+	let i = 0;
+	for (i = 0; i < names.length; i++) {
+		message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+	}
+	return message;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "Surprise"));
+function countDown(number) {
+	while (number >= 0) {
+		console.log(number);
+		number--;
+	}
+}
